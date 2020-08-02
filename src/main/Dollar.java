7af525866bb7988e5b2266683c44ed2137950f92
TaigaU121 <taiga.uji0121@gmail.com>
2020-08-02 23:07:35 +0900

    2章 レッド

--- src/main/Dollar.java
+++ src/main/Dollar.java
@@ -6,7 +6,8 @@ public class Dollar {
     public Dollar(int amount) {
         this.amount = amount;
     }
-    public void times(int multiplier) {
+    public Dollar times(int multiplier) {
         amount *= multiplier;
+        return null;
     }
 }
--- src/test/MoneyTest.java
+++ src/test/MoneyTest.java
@@ -11,8 +11,11 @@ class MoneyTest {
     public void testMultiplication() {
         Dollar five = new Dollar(5);
         five.times(2);
-        assertEquals(10, five.amount);
-        five.times(3);
-        assertEquals(15, five.amount);
+        /* amountが five.time(2),five.times(3)と上書きされることを防ぐため
+         timesメソッドからproductという新しいオブジェクトを返すようにする */
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
     }
 }
