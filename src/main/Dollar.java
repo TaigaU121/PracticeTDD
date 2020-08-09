e50476bc1379cf75a1cb5b024dcd094c15d47798
TaigaU121 <taiga.uji0121@gmail.com>
2020-08-09 16:42:40 +0900

    6章グリーン　Dollar内のメソッドを親クラスに引き上げたところまで

--- src/main/Dollar.java
+++ src/main/Dollar.java
@@ -1,17 +1,13 @@
 package main;
 
-public class Dollar {
+public class Dollar extends Money{
 
     private int amount; // amountを参照するクラスがDollarのみになったためprivateに変更
     public Dollar(int amount) {
-        this.amount = amount;
+//        this.amount = amount; //amountは通貨共通の変数としてMoneyクラスへ
     }
     public Dollar times(int multiplier) {
 //        return null; // コンパイル通すことが目的
         return new Dollar(amount * multiplier);
-    }
-    public boolean equals(Object object) {
-        Dollar dollar = (Dollar)object;
-        return amount == dollar.amount;
     }
 }
--- src/main/Money.java
+++ src/main/Money.java
@@ -1,5 +1,9 @@
 package main;
 
 public class Money {
-
+    protected int amount; //アクセス修飾子 protected は同一クラス、同一パッケージ、サブクラスから参照できる
+    public boolean equals(Object object) { // equals の引数にObject型を入れることでMoney型にキャストしたのちに金額を返す
+        Money money = (Money)object;
+        return amount == money.amount;
+    }
 }
